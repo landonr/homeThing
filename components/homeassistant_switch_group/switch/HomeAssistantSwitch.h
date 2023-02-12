8c7c31d303b5102b02ac86c1867d2ba8747fe979
landonr <landonroha@gmail.com>
2023-02-11 18:02:39 -0800

    remove unused format file and format

--- components/homeassistant_switch_group/switch/HomeAssistantSwitch.h
+++ components/homeassistant_switch_group/switch/HomeAssistantSwitch.h
@@ -1,20 +1,18 @@
 #pragma once
 
 #include <string>
+
 #include "DisplayUpdateInterface.h"
 #include "FriendlyNameEntity.h"
-#include "esphome/core/component.h"
 #include "esphome/components/api/api_server.h"
+#include "esphome/core/component.h"
 
 namespace esphome {
 namespace homeassistant_switch {
 
 class HomeAssistantSwitch : public switch_::Switch, public Component {
  public:
-  explicit HomeAssistantSwitch();
-  // explicit HomeAssistantSwitch(std::string newFriendlyName,
-  //                              std::string newEntityId,
-  //                              DisplayUpdateInterface *newCallback) { };
+  HomeAssistantSwitch();
   void set_entity_id(const std::string &entity_id) { entity_id_ = entity_id; }
   void set_attribute(const std::string &attribute) { attribute_ = attribute; }
   void setup() override;
