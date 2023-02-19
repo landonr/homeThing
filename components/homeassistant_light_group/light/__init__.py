e4390f02857aa33d736527b077b74a4bd21f2b91
landonr <landonroha@gmail.com>
2023-02-18 22:50:54 -0800

    make home assistant switch and light internal by default

--- components/homeassistant_light_group/light/__init__.py
+++ components/homeassistant_light_group/light/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light
-from esphome.const import CONF_ENTITY_ID, CONF_OUTPUT_ID, CONF_NAME, CONF_ID
+from esphome.const import CONF_ENTITY_ID, CONF_OUTPUT_ID, CONF_NAME, CONF_ID, CONF_INTERNAL
 
 homeassistant_light_ns = cg.esphome_ns.namespace("homeassistant_light")
 
@@ -15,14 +15,15 @@ CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend(
         cv.GenerateID(CONF_ID): cv.declare_id(HomeAssistantLightState),
         cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(HomeAssistantLight),
         cv.Required(CONF_ENTITY_ID): cv.entity_id,
-        cv.Required(CONF_NAME): cv.string
+        cv.Required(CONF_NAME): cv.string,
+        cv.Optional(CONF_INTERNAL, default=True): cv.boolean,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
-    cg.add(var.set_internal(True))
     cg.add(var.set_entity_id(config[CONF_ENTITY_ID]))
     cg.add(var.set_name(config[CONF_NAME]))
+    cg.add(var.set_internal(config[CONF_INTERNAL]))
     await cg.register_component(var, config)
     await light.register_light(var, config)
--- components/homeassistant_switch_group/switch/__init__.py
+++ components/homeassistant_switch_group/switch/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
-from esphome.const import CONF_ATTRIBUTE, CONF_ENTITY_ID, CONF_ID, CONF_NAME
+from esphome.const import CONF_ATTRIBUTE, CONF_ENTITY_ID, CONF_ID, CONF_NAME, CONF_INTERNAL
 
 homeassistant_switch_ns = cg.esphome_ns.namespace("homeassistant_switch")
 
@@ -15,15 +15,16 @@ CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend(
         cv.Required(CONF_ENTITY_ID): cv.entity_id,
         cv.Required(CONF_NAME): cv.string,
         cv.Optional(CONF_ATTRIBUTE): cv.string,
+        cv.Optional(CONF_INTERNAL, default=True): cv.boolean,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await switch.register_switch(var, config)
-
     cg.add(var.set_entity_id(config[CONF_ENTITY_ID]))
     cg.add(var.set_name(config[CONF_NAME]))
+    cg.add(var.set_internal(config[CONF_INTERNAL]))
     if CONF_ATTRIBUTE in config:
         cg.add(var.set_attribute(config[CONF_ATTRIBUTE]))
+    await cg.register_component(var, config)
+    await switch.register_switch(var, config)
