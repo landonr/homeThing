9ac9545a45eb9e67bef3ea4bab974a6cd55181d3
landonr <landonroha@gmail.com>
2023-03-12 23:57:05 -0700

    fix version newline

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -1,1 +1,10 @@
-#ifndef COMPONENTS_HOMETHING_VERSION_H_#define COMPONENTS_HOMETHING_VERSION_H_#include <string>namespace esphome {namespace homething_menu_base {static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup-279";}}  // namespace esphome#endif  // COMPONENTS_HOMETHING_VERSION_H_
+#ifndef COMPONENTS_HOMETHING_VERSION_H_
+#define COMPONENTS_HOMETHING_VERSION_H_
+#include <string>
+
+namespace esphome {
+namespace homething_menu_base {
+static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup-280";
+}
+}  // namespace esphome
+#endif  // COMPONENTS_HOMETHING_VERSION_H_
