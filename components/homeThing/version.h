d9355a105368f88ed842cc56cccd576669a206c8
landonr <landonroha@gmail.com>
2023-03-13 11:23:50 -0700

    fix workflow

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup/288-2023-03-13";
+static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup/289-2023-03-13";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
