7b968de23973bfcdec4f33b304566ca85163db94
landonr <landonroha@gmail.com>
2023-03-13 03:09:05 -0700

    update configs

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,7 @@
 echo "formatting"
 bash formatFiles.sh
 echo "building"
-for device in m5stickc m5stickcplus tdisplayt4 tdisplayremote fireremote homeThingTTGO
+for device in m5stickc m5stickcplus tdisplayt4 tdisplayremote fireremote homething-lilygo
 do
     echo building $device
     esphome run --no-logs $device.yaml
--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup-281";
+static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup-282";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
