b66c905713e7d6422330c5c0a4908bd94b73fbb0
landonr <landonroha@gmail.com>
2023-03-17 11:53:01 -0700

    fix action

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup/307-2023-03-17";
+static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup/308-2023-03-17";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
