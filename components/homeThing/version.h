95913d10d1ac08f740a1875b576ff955ad5968dc
landonr <landonroha@gmail.com>
2023-04-02 12:09:56 -0700

    remove extra package from example

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup/333-2023-04-02";
+static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup/341-2023-04-02";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
