3e7f31b16a995f4849376683117f7e351a14c715
landonr <landonroha@gmail.com>
2023-04-04 14:45:59 -0700

    update last device

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup/348-2023-04-04";
+static const std::string COMPONENTS_HOMETHING_VERSION = "lando/codeCleanup/349-2023-04-04";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
