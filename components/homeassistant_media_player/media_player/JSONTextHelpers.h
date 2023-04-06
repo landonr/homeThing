0fc1465051a3ca95158b06099ae24f5e9f41c263
landonr <landonroha@gmail.com>
2023-04-05 20:33:27 -0700

    remove commented code

--- components/homeassistant_media_player/media_player/JSONTextHelpers.h
+++ components/homeassistant_media_player/media_player/JSONTextHelpers.h
@@ -6,28 +6,6 @@
 namespace esphome {
 namespace homeassistant_media_player {
 
-// static std::vector<std::shared_ptr<MediaPlayerSource>> parseJsonDictionary(
-//     std::string state, std::string nameKey, std::string valueKey) {
-//   StaticJsonDocument<3072> doc;
-//   DeserializationError err = deserializeJson(doc, state);
-//   std::vector<std::shared_ptr<MediaPlayerSource>> sources;
-//   if (err) {
-//     ESP_LOGE("JSON", "deserializeJson() failed: ");
-//     ESP_LOGE("JSON", err.c_str());
-//     return sources;
-//   }
-//   JsonArray array = doc.as<JsonArray>();
-//   for (JsonVariant v : array) {
-//     std::string key = v[nameKey].as<std::string>();
-//     std::string value = v[valueKey].as<std::string>();
-//     ESP_LOGD("JSON", "new JSON object value %s %s", value.c_str(), key.c_str());
-//     auto newsource = std::make_shared<MediaPlayerSource>(
-//         PlaylistRemotePlayerSourceType, key, value);
-//     sources.push_back(newsource);
-//   }
-//   return sources;
-// }
-
 static std::string replaceAll(std::string str, const std::string& from,
                               const std::string& to) {
   size_t start_pos = 0;
