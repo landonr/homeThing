1b75a1a9234048f3b6da4d17b3c0f7aa11d65357
landonr <landonroha@gmail.com>
2023-04-28 19:13:33 +0000

    move components to its own repo

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "main/219-2023-04-27";
+static const std::string COMPONENTS_HOMETHING_VERSION = "landon/external_components/220-2023-04-28";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
