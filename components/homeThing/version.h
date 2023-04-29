8dbc905ebe51ffeadb7a824d0de7d7129fda3cd1
landonr <landonroha@gmail.com>
2023-04-29 14:23:02 +0000

    readd axp192 to sticks

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "landon/external_components/221-2023-04-29";
+static const std::string COMPONENTS_HOMETHING_VERSION = "landon/external_components/222-2023-04-29";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
