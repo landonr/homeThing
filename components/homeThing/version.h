fa048f857a1316a330eb216d6fa46ddd7175b0c2
landonr <landonroha@gmail.com>
2023-05-11 14:08:16 +0000

    add wifi to t4

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "lando/new-docs/232-2023-05-11";
+static const std::string COMPONENTS_HOMETHING_VERSION = "lando/new-docs/233-2023-05-11";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
