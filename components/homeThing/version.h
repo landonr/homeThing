d89f26f1de2aff5276ed25ac367d8ff90a14f8cc
Landon Rohatensky <landonroha@gmail.com>
2023-05-18 08:46:05 -0400

    add sleep after 45s if theres no network (#52)

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "main/235-2023-05-18";
+static const std::string COMPONENTS_HOMETHING_VERSION = "lando/sleep-when-no-network/237-2023-05-18";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
