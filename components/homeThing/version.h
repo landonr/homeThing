636362280f17de5f2578d4452f121f0b42d8a4f5
landonr <landonroha@gmail.com>
2023-05-28 15:48:27 +0000

    use remote components for stick

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "lando/update-devices/244-2023-05-28";
+static const std::string COMPONENTS_HOMETHING_VERSION = "lando/update-devices/245-2023-05-28";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
