d637d91e79728a824ecbabe8f92e77e7bb3e5d34
landonr <landonroha@gmail.com>
2023-05-28 15:38:17 +0000

    use stickc light

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,7 +4,7 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION = "lando/update-devices/242-2023-05-28";
+static const std::string COMPONENTS_HOMETHING_VERSION = "lando/update-devices/243-2023-05-28";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
