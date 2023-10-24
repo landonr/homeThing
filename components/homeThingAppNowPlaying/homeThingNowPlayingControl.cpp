ebf4e24fa802337eec56286cbe6371860959a690
landon rohatensky <landonroha@gmail.com>
2023-10-24 10:54:59 -0700

    fix now playing not going to root

--- components/homeThingAppNowPlaying/homeThingNowPlayingControl.cpp
+++ components/homeThingAppNowPlaying/homeThingNowPlayingControl.cpp
@@ -82,6 +82,10 @@ HomeThingMenuNowPlayingControl::app_menu_select(int index) {
            media_player_group_ == nullptr, menu_state_);
   switch (menu_state_) {
     case NOW_PLAYING_MENU_STATE_NOW_PLAYING:
+      // if (display_state_->get_draw_now_playing_bottom_menu()) {
+      //   return selectNowPlayingBottomMenu(
+      //       index);
+      // }
       break;
     case NOW_PLAYING_MENU_STATE_GROUPING:
       return homeThingNowPlayingMenuGroup::select_menu(index,
@@ -283,6 +287,9 @@ HomeThingMenuNowPlayingControl::selectNowPlayingBottomMenu(int index) {
       menu_state_ = NOW_PLAYING_MENU_STATE_GROUPING;
       return homething_menu_app::NavigationCoordination::
           NavigationCoordinationNone;
+    case homeassistant_media_player::MediaPlayerSupportedFeature::MENU_HOME:
+      return homething_menu_app::NavigationCoordination::
+          NavigationCoordinationRoot;
     default:
       break;
   }
