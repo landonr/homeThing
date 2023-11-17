c4dff2e7dbcafdb35b559cac75c6baf54e626950
Landon Rohatensky <landonroha@gmail.com>
2023-11-16 20:12:05 -0800

    add weather app (#93)
    * add weather app
    
    * add weather yaml
    
    * add launch image to weather
    
    * add weather to tdisplay

---
+++ components/homeThingAppWeather/HomeThingAppWeather.cpp
@@ -0,0 +1,152 @@
+#include "HomeThingAppWeather.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace homething_app_weather {
+void HomeThingAppWeather::rootMenuTitles(
+    std::vector<homething_menu_base::MenuTitleBase*>* menu_titles) {
+  menu_titles->push_back(new homething_menu_base::MenuTitleBase(
+      "Weather", "", homething_menu_base::ArrowMenuTitleRightIcon));
+}
+
+void HomeThingAppWeather::app_menu_titles(
+    std::vector<homething_menu_base::MenuTitleBase*>* menu_titles) {}
+
+// menu screens
+homething_menu_app::NavigationCoordination HomeThingAppWeather::app_menu_select(
+    int index) {
+  return homething_menu_app::NavigationCoordination::NavigationCoordinationNone;
+}
+
+bool HomeThingAppWeather::should_draw_app() {
+  return true;
+}
+
+void HomeThingAppWeather::display_temperature(int xPos, int yPos,
+                                              float temperature) {
+  // Display temperature icon
+  // display_buffer_->drawIcon(10, 10, temperature_icon_data, temperature_icon_width, temperature_icon_height, display_state_->get_color_palette()->get_accent_primary());
+  // Draw temperature information
+  auto state = "Temperature: " + to_string(static_cast<int>(temperature)) + "C";
+  display_state_->drawTextWrapped(
+      xPos, yPos, display_state_->get_font_medium(),
+      display_state_->get_color_palette()->get_accent_primary(),
+      display::TextAlign::TOP_CENTER, state.c_str(), 2, display_buffer_);
+}
+
+void HomeThingAppWeather::display_humidity(int xPos, int yPos, float humidity) {
+  auto state = "Humidity: " + to_string(static_cast<int>(humidity)) + "%%";
+  display_state_->drawTextWrapped(
+      xPos, yPos, display_state_->get_font_medium(),
+      display_state_->get_color_palette()->get_accent_primary(),
+      display::TextAlign::TOP_CENTER, state.c_str(), 2, display_buffer_);
+}
+
+void HomeThingAppWeather::display_condition(int xPos, int yPos,
+                                            const std::string& condition) {
+  display_state_->drawTextWrapped(
+      xPos, yPos, display_state_->get_font_medium(),
+      display_state_->get_color_palette()->get_accent_primary(),
+      display::TextAlign::CENTER_LEFT, condition, 2, display_buffer_);
+}
+
+void HomeThingAppWeather::display_condition_image(
+    int xPos, int yPos, const std::string& condition) {
+  int newXPos = xPos - cloudy_image_->get_width() / 2 - 10;
+  if (condition == "cloudy") {
+    display_buffer_->image(newXPos, yPos, cloudy_image_,
+                           display::ImageAlign::TOP_CENTER,
+                           Color(255, 255, 255));
+  } else if (condition == "fog") {
+    display_buffer_->image(newXPos, yPos, fog_image_,
+                           display::ImageAlign::TOP_CENTER,
+                           Color(255, 255, 255));
+  } else if (condition == "snow") {
+    display_buffer_->image(newXPos, yPos, snow_image_,
+                           display::ImageAlign::TOP_CENTER,
+                           Color(255, 255, 255));
+  } else if (condition == "sunny") {
+    display_buffer_->image(newXPos, yPos, sunny_image_,
+                           display::ImageAlign::TOP_CENTER,
+                           Color(255, 255, 255));
+  } else if (condition == "rainy") {
+    display_buffer_->image(newXPos, yPos, rainy_image_,
+                           display::ImageAlign::TOP_CENTER,
+                           Color(255, 255, 255));
+  } else if (condition == "night") {
+    display_buffer_->image(newXPos, yPos, night_image_,
+                           display::ImageAlign::TOP_CENTER,
+                           Color(255, 255, 255));
+  }
+}
+
+void HomeThingAppWeather::draw_app(
+    int menuIndex,
+    const std::vector<homething_menu_base::MenuTitleBase*>* active_menu) {
+  int centerX = display_buffer_->get_width() / 2;
+  int imageXPos = centerX;
+  int imageYPos = display_state_->get_header_height() + 2;
+  int textXPos = centerX;
+  int textYPos = imageYPos + cloudy_image_->get_height() + 8;
+  int textSpacing = display_state_->get_font_medium()->get_height() + 2;
+
+  // Display temperature, humidity, status, and weather condition with icons/images
+  display_condition_image(imageXPos, imageYPos, condition_sensor_->state);
+  display_condition(textXPos + 10,
+                    imageYPos + (cloudy_image_->get_height() / 2),
+                    condition_sensor_->state);
+  display_temperature(textXPos, textYPos, temperature_sensor_->state);
+  display_humidity(textXPos, textYPos + textSpacing, humidity_sensor_->state);
+}
+
+bool HomeThingAppWeather::idleTick(int idleTime, int display_timeout) {
+  return false;
+}
+
+int HomeThingAppWeather::root_menu_size() {
+  return 1;
+}
+void HomeThingAppWeather::reset_menu() {}
+void HomeThingAppWeather::set_app_menu_index(int app_menu_index) {}
+
+// buttons
+homething_menu_app::NavigationCoordination
+HomeThingAppWeather::rotaryScrollClockwise(int rotary) {
+  return homething_menu_app::NavigationCoordination::NavigationCoordinationNone;
+}
+homething_menu_app::NavigationCoordination
+HomeThingAppWeather::rotaryScrollCounterClockwise(int rotary) {
+  return homething_menu_app::NavigationCoordination::NavigationCoordinationNone;
+}
+homething_menu_app::NavigationCoordination
+HomeThingAppWeather::buttonPressUp() {
+  return homething_menu_app::NavigationCoordination::NavigationCoordinationPop;
+}
+
+homething_menu_app::NavigationCoordination
+HomeThingAppWeather::buttonPressDown() {
+  return homething_menu_app::NavigationCoordination::NavigationCoordinationNone;
+}
+
+homething_menu_app::NavigationCoordination
+HomeThingAppWeather::buttonPressLeft() {
+  return homething_menu_app::NavigationCoordination::NavigationCoordinationNone;
+}
+
+homething_menu_app::NavigationCoordination
+HomeThingAppWeather::buttonPressRight() {
+  return homething_menu_app::NavigationCoordination::NavigationCoordinationNone;
+}
+
+homething_menu_app::NavigationCoordination
+HomeThingAppWeather::buttonPressSelect(int menuIndex) {
+  return homething_menu_app::NavigationCoordination::NavigationCoordinationNone;
+}
+
+homething_menu_app::NavigationCoordination
+HomeThingAppWeather::buttonPressOption() {
+  return homething_menu_app::NavigationCoordination::NavigationCoordinationNone;
+}
+
+}  // namespace homething_app_weather
+}  // namespace esphome
---
+++ components/homeThingAppWeather/__init__.py
@@ -0,0 +1,61 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+from esphome.components import homeThingApp, sensor, text_sensor, image
+
+AUTO_LOAD = ["homeThingApp"]
+homething_app_weather_ns = cg.esphome_ns.namespace("homething_app_weather")
+
+HomeThingAppWeather = homething_app_weather_ns.class_("HomeThingAppWeather", cg.Component)
+
+CONF_TEMPERATURE_SENSOR = "temperature_sensor"
+CONF_HUMIDITY_SENSOR = "humidity_sensor"
+CONF_CONDITION_SENSOR = "condition_sensor"
+CONF_CLOUDY = "cloudy"
+CONF_FOG = "fog"
+CONF_SNOW = "snow"
+CONF_SUNNY = "sunny"
+CONF_RAINY = "rainy"
+CONF_NIGHT = "night"
+CONF_IMAGES = "images"
+
+IMAGE_SCHEMA = cv.Schema(
+    {
+        cv.Required(CONF_CLOUDY): cv.use_id(image.Image_),
+        cv.Required(CONF_FOG): cv.use_id(image.Image_),
+        cv.Required(CONF_SNOW): cv.use_id(image.Image_),
+        cv.Required(CONF_SUNNY): cv.use_id(image.Image_),
+        cv.Required(CONF_RAINY): cv.use_id(image.Image_),
+        cv.Required(CONF_NIGHT): cv.use_id(image.Image_),
+    }
+)
+
+
+CONFIG_SCHEMA = homeThingApp.BASE_SCHEMA.extend(
+    {
+        cv.GenerateID(CONF_ID): cv.declare_id(HomeThingAppWeather),
+        cv.Required(CONF_TEMPERATURE_SENSOR): cv.use_id(sensor.Sensor),
+        cv.Required(CONF_HUMIDITY_SENSOR): cv.use_id(sensor.Sensor),
+        cv.Required(CONF_CONDITION_SENSOR): cv.use_id(text_sensor.TextSensor),
+        cv.Required(CONF_IMAGES): IMAGE_SCHEMA,
+    }
+)
+
+async def to_code(config):
+    var = await homeThingApp.new_app_base(config)
+    await cg.register_component(var, config)
+
+    temperature_sensor = await cg.get_variable(config[CONF_TEMPERATURE_SENSOR])
+    humidity_sensor = await cg.get_variable(config[CONF_HUMIDITY_SENSOR])
+    condition_sensor = await cg.get_variable(config[CONF_CONDITION_SENSOR])
+    cg.add(var.set_temperature_sensor(temperature_sensor))
+    cg.add(var.set_humidity_sensor(humidity_sensor))
+    cg.add(var.set_condition_sensor(condition_sensor))
+
+    images = config[CONF_IMAGES]
+    cg.add(var.set_cloudy_image(await cg.get_variable(images[CONF_CLOUDY])))
+    cg.add(var.set_fog_image(await cg.get_variable(images[CONF_FOG])))
+    cg.add(var.set_snow_image(await cg.get_variable(images[CONF_SNOW])))
+    cg.add(var.set_sunny_image(await cg.get_variable(images[CONF_SUNNY])))
+    cg.add(var.set_rainy_image(await cg.get_variable(images[CONF_RAINY])))
+    cg.add(var.set_night_image(await cg.get_variable(images[CONF_NIGHT])))
