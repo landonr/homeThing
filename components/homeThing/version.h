a60f8cb91de957c38c2673332bda393c087426c7
landon rohatensky <landonroha@gmail.com>
2024-04-06 18:32:11 -0700

    add improv wifi to boot

--- components/homeThing/version.h
+++ components/homeThing/version.h
@@ -4,8 +4,8 @@
 
 namespace esphome {
 namespace homething_menu_base {
-static const std::string COMPONENTS_HOMETHING_VERSION =
-    "lando/boot_image/2023-12-30";
+static const std::string COMPONENTS_HOMETHING_VERSION = "improv-wifi/2024-04-06";
 }
 }  // namespace esphome
 #endif  // COMPONENTS_HOMETHING_VERSION_H_
+
